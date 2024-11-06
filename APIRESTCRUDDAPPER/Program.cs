d964f2a76160f4903d3c319dc1d2545cb0f43390
Felipe <fgcabral@yahoo.com>
2024-11-06 19:26:58 -0300

    Implementação do Log - Serilog

--- APIRESTCRUDDAPPER/Program.cs
+++ APIRESTCRUDDAPPER/Program.cs
@@ -2,8 +2,17 @@ using APIRESTCRUDDAPPER.Application.Profiles.Profiles;
 using APIRESTCRUDDAPPER.Domain.Interfaces;
 using APIRESTCRUDDAPPER.Services;
 using Microsoft.OpenApi.Models;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configura o Serilog para usar a configuração do appsettings.json
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console() // Saída de logs no console
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day) // Saída de logs em arquivos
+    .CreateLogger();
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -12,19 +21,11 @@ builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo 
     { 
-        Title = "API REST CRUD DAPPER",
+        Title = "API REST",
         Version = "v1",
-        Description = "Api REST CRUD Utilizando Dapper + SQLServer",
-        Contact = new OpenApiContact
-        {
-            Name = "Felipe Cabral",
-            Url = new Uri("https://github.com/fgcabral88")
-        },
-        License = new OpenApiLicense
-        {
-            Name = "Licença MIT",
-            Url = new Uri("https://opensource.com/licenses/MIT")
-        },
+        Description = "CRUD Utilizando Dapper + AutoMapper + SQLServer + Log Serilog",
+        Contact = new OpenApiContact { Name = "Felipe Cabral", Url = new Uri("https://github.com/fgcabral88") },
+        License = new OpenApiLicense { Name = "Licença MIT", Url = new Uri("https://opensource.com/licenses/MIT") },
         TermsOfService = new Uri("https://opensource.com/terms-of-service")
     });
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "APIRESTCRUDDAPPERAnnotation.xml"));
@@ -34,6 +35,7 @@ builder.Services.AddScoped<IUsuarioInterface, UsuarioService>();
 
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddAutoMapper(typeof(ProfileAutoMapper));
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
