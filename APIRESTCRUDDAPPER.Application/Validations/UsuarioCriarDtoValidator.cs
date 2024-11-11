1869de10605f8845e0bc72facd2afe16718ebe25
Felipe <fgcabral@yahoo.com>
2024-11-11 12:35:40 -0300

    #feat: Ajustes no validator e configurar a injeção na program do editarValidator

--- APIRESTCRUDDAPPER.Application/Validations/UsuarioCriarDtoValidator.cs
+++ APIRESTCRUDDAPPER.Application/Validations/UsuarioCriarDtoValidator.cs
@@ -24,25 +24,36 @@ namespace APIRESTCRUDDAPPER.Application.Validations
             RuleFor(x => x.Cargo)
                 .NotNull()
                 .NotEmpty()
-                .WithMessage("O cargo é obrigatório.");
+                .WithMessage("O cargo é obrigatório.")
+                .Length(3, 100)
+                .WithMessage("O cargo deve ter entre 3 e 100 caracteres.");
 
             RuleFor(x => x.Salario)
                 .NotNull()
                 .NotEmpty()
-                .WithMessage("O salário é obrigatório.");
+                .WithMessage("O salário é obrigatório.")
+                .GreaterThan(0)
+                .WithMessage("O salário deve ser maior que zero.");
 
             RuleFor(x => x.CPF)
                 .NotNull()
                 .NotEmpty()
                 .WithMessage("O CPF é obrigatório.")
                 .Length(11)
-                .WithName("CPF");
+                .WithName("CPF")
+                .Matches("^[0-9]*$")
+                .WithMessage("O CPF deve conter apenas números.");
 
             RuleFor(x => x.Senha)
+                .NotNull()
                 .NotEmpty()
                 .WithMessage("A senha é obrigatória.")
                 .Length(3,8)
-                .WithMessage("A senha deve ter no mínimo 3 e máximo 8 caracteres.");
+                .WithMessage("A senha deve ter no mínimo 3 e máximo 8 caracteres.")
+                .Matches("[A-Z]")
+                .WithMessage("A senha deve conter pelo menos uma letra maiúscula.")
+                .Matches("[^a-zA-Z0-9]")
+                .WithMessage("A senha deve conter pelo menos um caractere especial.");
         }
     }
 }
--- APIRESTCRUDDAPPER.Application/Validations/UsuarioEditarDtoValidator.cs
+++ APIRESTCRUDDAPPER.Application/Validations/UsuarioEditarDtoValidator.cs
@@ -10,7 +10,9 @@ namespace APIRESTCRUDDAPPER.Application.Validations
             RuleFor(x => x.Id)
                 .NotNull()
                 .NotEmpty()
-                .WithMessage("O id é obrigatório.");
+                .WithMessage("O id é obrigatório.")
+                .GreaterThan(0)
+                .WithMessage("O id deve ser maior que zero.");
 
             RuleFor(x => x.NomeCompleto)
                 .NotNull()
@@ -29,19 +31,25 @@ namespace APIRESTCRUDDAPPER.Application.Validations
             RuleFor(x => x.Cargo)
                 .NotNull()
                 .NotEmpty()
-                .WithMessage("O cargo é obrigatório.");
+                .WithMessage("O cargo é obrigatório.")
+                .Length(3, 100)
+                .WithMessage("O cargo deve ter entre 3 e 100 caracteres.");
 
             RuleFor(x => x.Salario)
                 .NotNull()
                 .NotEmpty()
-                .WithMessage("O salário é obrigatório.");
+                .WithMessage("O salário é obrigatório.")
+                .GreaterThan(0)
+                .WithMessage("O salário deve ser maior que zero.");
 
             RuleFor(x => x.CPF)
-                .NotNull()
-                .NotEmpty()
-                .WithMessage("O CPF é obrigatório.")
-                .Length(11)
-                .WithName("CPF");
+               .NotNull()
+               .NotEmpty()
+               .WithMessage("O CPF é obrigatório.")
+               .Length(11)
+               .WithName("CPF")
+               .Matches("^[0-9]*$")
+               .WithMessage("O CPF deve conter apenas números.");
         }
     }
 }
